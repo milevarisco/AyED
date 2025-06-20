77ffbb53e1f6d08767ac69fb19c2bfe3c1cdf4de
milevarisco <mvarisco@frba.utn.edu.ar>
2025-06-20 18:15:19 -0300

    arrays

--- ARRAYS/vectores 1/vec1.cpp
+++ ARRAYS/vectores 1/vec1.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main(){
-    cout << "Ingrese 10 valores:" << endl;
-
     int valores[10];
+    
+    cout << "Ingrese 10 valores:" << endl;
     for (int i = 0; i < 10; i++){
         cout << "valor " << i + 1 << ": ";
         cin >> valores[i];
